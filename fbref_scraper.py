3b2f19d41f0ef8b78b5bf1361f24e6655cac6178
Lewis Conroy <lconroy2017@gmail.com>
2024-12-12 12:28:29 +0000

    added recent years and removed bug where someplayers missing but removed keeper data

--- fbref_scraper.py
+++ fbref_scraper.py
@@ -19,8 +19,8 @@ def get_data_info():
     
     while True:
         # select league [    / La Liga / Serie A / Ligue 1 / Bundesliga]
-        league = input('Select League (Premier League / La Liga / Serie A / Ligue 1 / Bundesliga): ')
-        
+        # league = input('Select League (Premier League / La Liga / Serie A / Ligue 1 / Bundesliga): ')
+        league = 'Premier League'
         # check if input valid
         if league not in leagues:
             print('League not valid, try again')
@@ -50,8 +50,8 @@ def get_data_info():
             
     while True: 
         # select season after 2017 as XG only available from 2017,
-        season = input('Select Season (2017-2018, 2018-2019, 2019-2020, 2020-2021, 2021-2022, 2022-2023, 2023-2024 : ')
-        
+        # season = input('Select Season (2017-2018, 2018-2019, 2019-2020, 2020-2021, 2021-2022, 2022-2023, 2023-2024 : ')
+        season = '2019-2020'
         # check if input valid
         if season not in seasons:
             print('Season not valid, try again')
@@ -99,49 +99,45 @@ def get_match_links(url, league):
     return match_links
 
 
-def player_data(match_links, league, season):
-    # loop through all fixtures
-    player_data = pd.DataFrame([])
-    for count, link in enumerate(match_links):
+def player_data(match_links,leauge,season):
+  # loop through all fixtures
+  player_data = pd.DataFrame([])
+  for count, link in enumerate(match_links):
+    try:
+      tables = pd.read_html(link)
+      for table in tables:
         try:
-            tables = pd.read_html(link)
-            for table in tables:
-                try:
-                    table.columns = table.columns.droplevel()
-                except Exception:
-                    continue
+          table.columns = table.columns.droplevel()
+        except Exception:
+          continue
+        
 
-            # get player data
-            def get_team_1_player_data():
-                # outfield and goal keeper data stored in seperate tables 
-                data_frames = [tables[3], tables[9]]
-                
-                # merge outfield and goal keeper data
-                df = reduce(lambda left, right: pd.merge(left, right, 
-                    on=['Player', 'Nation', 'Age', 'Min'], how='outer'), data_frames).iloc[:-1]
-                
-                # assign a home or away value
-                return df.assign(home=1, game_id=count)
+      # get player data
+      def get_team_1_player_data():
+        # outfield and goal keeper data stored in seperate tables 
+        data_frames = [tables[3], tables[9]]
+        df = data_frames[0]
+        # assign a home or away value
+        return df.assign(home=1, game_id=count)
 
-            # get second teams  player data        
-            def get_team_2_player_data():
-                data_frames = [tables[10], tables[16]]
-                df = reduce(lambda left, right: pd.merge(left, right,
-                    on=['Player', 'Nation', 'Age', 'Min'], how='outer'), data_frames).iloc[:-1]
-                return df.assign(home=0, game_id=count)
+      # get second teams  player data        
+      def get_team_2_player_data():
+        data_frames = [tables[10], tables[16]]
+        df = data_frames[0]
+        return df.assign(home=0, game_id=count)
 
-            # combine both team data and export all match data to csv
-            t1 = get_team_1_player_data()
-            t2 = get_team_2_player_data()
-            player_data = pd.concat([player_data, pd.concat([t1,t2]).reset_index()])
-            
-            print(f'{count+1}/{len(match_links)} matches collected')
-            player_data.to_csv(f'{league.lower()}_{season.lower()}_player_data.csv', 
-                header=True, index=False, mode='w')
-        except:
-            print(f'{link}: error')
-        # sleep for 3 seconds after every game to avoid IP being blocked
-        time.sleep(3)
+      # combine both team data and export all match data to csv
+      t1 = get_team_1_player_data()
+      t2 = get_team_2_player_data()
+      player_data = pd.concat([player_data, pd.concat([t1,t2]).reset_index()])
+      
+      print(f'{count+1}/{len(match_links)} matches collected')
+      player_data.to_csv(f'{leauge}_{season}_data.csv', 
+          header=True, index=False, mode='w')
+    except:
+      print(f'{link}: error')
+    # sleep for 3 seconds after every game to avoid IP being blocked
+    time.sleep(3)
 
 
 # main function
